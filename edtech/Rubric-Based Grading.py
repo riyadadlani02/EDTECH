b46663cb272a413c47f86ee64d16ed4dc6b1aebb
riyadadlani02 <113050898+riyadadlani02@users.noreply.github.com>
2023-11-21 14:23:00 +0530

    Update Rubric-Based Grading.py

--- edtech/Rubric-Based Grading.py
+++ edtech/Rubric-Based Grading.py
@@ -1,6 +1,5 @@
 def rubric_based_grading(student_answers, rubric):
     
-    scores = {}  # Store scores for each question
-    for 
-        scores[question] = calculate_score(answer, rubric[question])
+    scores = {}  
+    for scores[question] = calculate_score(answer, rubric[question])
     return scores
